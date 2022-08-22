37cdc82b4ae0e428664675bd45b4b3cc85a14ea7
Drew E. Winters <53614162+drewwint@users.noreply.github.com>
2022-08-21 22:16:11 -0600

    Add files via upload

---
+++ NDA_3165_BIDS_Downloader_Script.sh
@@ -0,0 +1,10 @@
+
+
+python3 download.py 
+  -i /mnt/d/Collection_3165/Package_1202418/datastructure_manifest.txt
+  -o /mnt/d/Collection_3165/dat_3165/ 
+  -s /mnt/d/Collection_3165/subject_list.txt 
+  -l /mnt/d/Collection_3165/log_3165 
+  -d /mnt/d/Collection_3165/nda-abcd-s3-downloader-master/data_subsets.txt 
+  -p 4
+
